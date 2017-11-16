20688331b9deff2679ab98e799b7d545fb641062
minigear <coosac@google.com>
2017-11-16 09:31:02 +0900

    test git hub push

---
+++ src/main/java/com/chatbot/chatbottest/ChatbottestApplication.java
@@ -0,0 +1,12 @@
+package com.chatbot.chatbottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbottestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbottestApplication.class, args);
+	}
+}
